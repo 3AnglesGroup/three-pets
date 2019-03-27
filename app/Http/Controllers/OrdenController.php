f63a8cda10a04f6985be13ddf1bfe5e8e7aba509
John Alvaro <jhon54plex@hotmail.com>
2019-03-27 16:19:42 -0500

    Configurando pagina de confirmacion

--- app/Http/Controllers/OrdenController.php
+++ app/Http/Controllers/OrdenController.php
@@ -9,14 +9,12 @@ class OrdenController extends Controller
 {
    public function confirmacion(Request $request){
 
-    $orden = new Orden();
-    $orden->nombre = 'llegando';
-    $orden->save();
-
         if($request['state_pol'] == 4){
           $orden = Orden::where('referencia',$request['reference_pol'])->first(); 
           $orden->estado = true;
-          $orden->save(); 
+          $orden->save();
+          return 200;
         }
+        return 200;
    }
 }
