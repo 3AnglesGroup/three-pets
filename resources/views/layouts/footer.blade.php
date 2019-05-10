166498f833106c8bad52b9c3a22810e25a705382
John Alvaro <jhon54plex@hotmail.com>
2019-05-10 16:27:11 -0500

    Agregando cambios redireccionamiento a redes sociales

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -10,7 +10,7 @@
                     <div class="box">
                         <p class="brand"><img src="img/three-pets-logo.svg" width="70"> THREE PETS COLOMBIA</p>
                         <p>Síguenos en:</p>
-                        <p class="social"><a href="#"><img src="img/facebook.svg"></a> <a href="#"><img src="img/instagram.svg"></a> @ThreePetsColombia</p>
+                        <p class="social"><a href="https://www.facebook.com/ThreepetsColombia/"><img src="img/facebook.svg"></a> <a href="https://www.instagram.com/threepets_colombia/?hl=es-la"><img src="img/instagram.svg"></a> @ThreePetsColombia</p>
                         <p class="contact"><strong>Cel.</strong> 316 308 5340<br><strong>E-mail.</strong> info@three-pets.com.co</p>
                     </div>
                     <span class="copyright">Copyright © 2019 Three Pets Colombia - Created by <img src="img/3anglesgroup.svg"></span>
